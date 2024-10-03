479d5cef5b3480fa6783485d0445816454c52eba
Shrawan2002 <shrawankumar1754@gmail.com>
2024-10-03 23:20:39 +0530

    initial files

--- todoApp/app.js
+++ todoApp/app.js
@@ -4,7 +4,29 @@ let btn = document.querySelector("button");
 
 btn.addEventListener("click", function(){
     let item = document.createElement("li");
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
     item.innerText = inp.value;
+    item.appendChild(delBtn);
     ul.appendChild(item);
     inp.value = "";
 })
+
+
+ul.addEventListener("click", function(event){
+    console.dir(event.target);
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+})
+
+// let delBtns = document.querySelectorAll(".delete");
+// for(delBtn of delBtns){
+//     delBtn.addEventListener("click", function(){
+//         console.log("button was click!");
+//         let par = this.parentElement;
+//         par.remove();
+//     })
+// }

0a7414ca9679727005595edfe9a768d7d44ed2ce
Shrawan2002 <shrawankumar1754@gmail.com>
2024-10-07 09:15:55 +0530

    initial files

---
+++ ErrorHandling/app.js
@@ -0,0 +1,22 @@
+let h1 = document.querySelector("h1");
+
+
+function getChange(color,delay){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            let num = Math.floor(Math.random()*10);
+            if(num > 3){
+                reject("promise rejected");
+            }
+                h1.style.color = color;
+                resolve("promise was resolve");
+           
+        }, delay);
+    });
+}
+
+ async function demo(){
+   await getChange("red", 1000);
+   await getChange("green", 1000);
+   await getChange("yellow", 1000);
+}

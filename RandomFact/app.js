87d5e288668344ea5f7a21f223f28e438f47ccb6
Shrawan2002 <shrawankumar1754@gmail.com>
2024-10-09 22:23:48 +0530

    initial files

--- RandomFact/app.js
+++ RandomFact/app.js
@@ -8,7 +8,7 @@ let url = 'https://v2.jokeapi.dev/joke/Any?safe-mode';
 async function getFact() {
     try{
         let fact = await axios.get(url);
-    console.log(fact.setup);
+    console.log(JSON.parse(fact.data.joke).setup);
     }catch(err){
         console.log(err);
     }

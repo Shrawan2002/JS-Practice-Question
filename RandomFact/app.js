6a3d99a83f3c421dd0fb177e36abd8f7b4105515
Shrawan2002 <shrawankumar1754@gmail.com>
2024-10-10 11:12:55 +0530

    initial files

--- RandomFact/app.js
+++ RandomFact/app.js
@@ -1,15 +1,23 @@
 let btn = document.querySelector("button");
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click",async function(){
     console.log("click me");
+    let result = await getFact();
+    console.log(result);
+    let para = document.querySelector("#random");
+    para.innerText = result;
 })
 
 let url = 'https://official-joke-api.appspot.com/random_joke';
 async function getFact() {
     try{
-        let fact = await axios.get(url);
-    console.log(fact.id);
+        let fact = await fetch(url);
+        let  data = await fact.json();
+    // console.log(data.punchline);
+    // console.log(data.setup);
+    return data.setup;
     }catch(err){
         console.log(err);
+        return "page is not found";
     }
 }

bdc54ced4dfaa352541e2a21186c11d196a977dd
Shrawan2002 <shrawankumar1754@gmail.com>
2024-12-05 11:33:51 +0530

    initial files

--- Practice First/app.js
+++ Practice First/app.js
@@ -15,3 +15,12 @@ body.appendChild(para);
 let h3 = document.createElement("h3");
 h3.innerText = "I am a blue h3";
 body.appendChild(h3);
+
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let para1 = document.createElement("p");
+h1.innerText = " I am  in a div";
+para1.innerText = "ME Too";
+div.appendChild(h1);
+div.appendChild(para1);
+body.appendChild(div);
